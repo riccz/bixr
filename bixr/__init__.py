98d7ec05ef2f5a56572535a79bcee3dcf142f425
Riccardo Zanol <ricc@zanl.eu>
2021-06-25 16:08:56 +0200

    Add more package metadata

--- bixr/__init__.py
+++ bixr/__init__.py
@@ -1,2 +1,6 @@
 from .llapi import LLAPI
 from .api import API
+
+__version__ = "0.0.1"
+__author__ = "Riccardo Zanol <ricc@zanl.eu>"
+__all__ = [LLAPI, API]
--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
-from setuptools import setup, find_packages
+import io
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding="utf-8") as fd:
+        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
+
 
 setup(
     name="bixr",
     version="0.0.1",
-    packages=find_packages(include=["bixr"]),
+    url="https://github.com/riccz/bixr",
+    license="MIT",
+    author="Riccardo Zanol",
+    author_email="ricc@zanl.eu",
+    description="Exchange rate API from Banca d'Italia",
+    long_description=read("README.rst"),
+    packages=find_packages(exclude=("tests",)),
     install_requires=[
         "requests",
         "requests-cache",
@@ -11,4 +29,11 @@ setup(
     extras_require={
         "pandas": ["pandas"],
     },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
